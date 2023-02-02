199a820a5f51f4e863efe2a54b0ff44e24d87b50
Brandon F. Creed <brandon.f.creed@gmail.com>
2023-02-02 16:04:47 -0500

    added a cfehome app inside project, added a model, view, and route

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,8 +4,9 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={ "product_id": 4242 })
-# print(get_response.text)
+get_response = requests.get(endpoint, json={ "product_id": 4242 })
+print(get_response.headers)
+print(get_response.text)
 # print(get_response.status_code)
-print(get_response.json())
+# print(get_response.json())
 # print(get_response.status_code)
