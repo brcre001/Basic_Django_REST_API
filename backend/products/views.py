6c0eead77eeb1811858a63ce155b95d2bc9d5dad
Brandon F. Creed <brandon.f.creed@gmail.com>
2023-02-08 14:35:08 -0500

    using mixins to create more of a general class

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # from django.http import Http404
@@ -10,6 +10,7 @@ from .serializers import ProductSerializer
 # Django has generic views Create, Detail, List, Update, Destroy
 # They can be combined to make the code simpler or
 # they can be kept separate.
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -25,14 +26,18 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         serializer.save(content=content)
         # send a Django signal
 
+
 product_list_create_view = ProductListCreateAPIView.as_view()
+
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk' ??
 
+
 product_detail_view = ProductDetailAPIView.as_view()
+
 
 class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
@@ -43,9 +48,11 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
         instance = serializer.save()
         if not instance.content:
             instance.content = instance.title
-            ## 
+            ##
+
 
 product_update_view = ProductUpdateAPIView.as_view()
+
 
 class ProductDestroyAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
@@ -53,20 +60,50 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
     lookup_field = 'pk'
 
     def perform_destroy(self, instance):
-        ## instance
+        # instance
         super().perform_destroy(instance)
+
 
 product_destroy_view = ProductDestroyAPIView.as_view()
 
-class ProductListAPIView(generics.ListAPIView):
-    '''
-    Not going to use this method
-    '''
+# class ProductListAPIView(generics.ListAPIView):
+#     '''
+#     Not going to use this method
+#     '''
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     # lookup_field = 'pk' ??
+
+# product_list_view = ProductListAPIView.as_view()
+
+
+class ProductMixinView(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.RetrieveModelMixin, generics.GenericAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # lookup_field = 'pk' ??
+    lookup_field = 'pk'
 
-product_list_view = ProductListAPIView.as_view()
+    def get(self, request, *args, **kwargs):  # HTTP -> get
+        print(args, kwargs)
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
+
+
+product_mixin_view = ProductMixinView.as_view()
+
 
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *arg, **kwargs):
@@ -74,7 +111,7 @@ def product_alt_view(request, pk=None, *arg, **kwargs):
 
     if method == "GET":
         if pk is not None:
-            obj = get_object_or_404(Product, pk=pk) # Http404
+            obj = get_object_or_404(Product, pk=pk)  # Http404
             data = ProductSerializer(obj, many=False).data
             return Response(data)
         # list view
